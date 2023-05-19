80fb933e3919aabde5889f5a6afacf5303903951
Axis <77634274+blankRiot96@users.noreply.github.com>
2023-05-19 18:22:51 +0530

    (enhance) updated version info in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 # py -m build
 # twine upload dist/*
-VERSION = "0.1.0"
+VERSION = "0.2.0"
 DESCRIPTION = "A tool to find the most common libraries used"
 LONG_DESCRIPTION = """
 A tool to find the most commonly imported
